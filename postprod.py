dc597c78f708f976e3ade1f79915c7ca62bb6cd5
d2ns <d2ns@das-labor.org>
2016-05-03 13:36:09 +0200

    etherpad-support

--- postprod.py
+++ postprod.py
@@ -1,12 +1,13 @@
-#!/bin/python2
+#!/bin/python
 import os, sys, re, glob
 import datetime
 from datetime import timedelta
+import urllib.request
 
 ## To test the script (to not write files or convert them) you can set DEBUG to True
 DEBUG = True
 
-## Configs
+## Configs ##
 audiofiles_folder = "/home/dennis/Audio" # without "/" at the end
 
 # 1. shortcut, 2. filename/broadcast
@@ -15,10 +16,12 @@ broadcasts = {
               'pt': 'primetime',
               'ff': 'faldriansfeierabend'
              }
+etherpad_url = "http://pad.theradio.cc/p"
+## End Configs ##
 
 ## Set the date
 
-input_year = raw_input("Jahr? (z.B. 2016, fuer heutiges Datum einfach <ENTER>) > ")
+input_year = input("Jahr? (z.B. 2016, fuer heutiges Datum einfach <ENTER>) > ")
 day = ''
 if input_year == "":
     year = datetime.date.today().year
@@ -26,12 +29,11 @@ if input_year == "":
     day = datetime.date.today().strftime('%d')
 else:
     while day == '':
-        input_month = raw_input("Monat? (z.B. 04) > ")
+        input_month = input("Monat? (z.B. 04) > ")
         input_day = input("Tag? (z.B. 19) > ")
         year = input_year
         month = input_month
         day = input_day
-        print(str(year) + str(month) + str(day))
 
 print("\033[1m# Datum: %s.%s.%s\033[0m" % (day, month, year))
 
@@ -65,22 +67,21 @@ elif listfile == []:
 
 broadcast = ""
 while broadcast == "":
-    input_broadcast = raw_input("Sendung? " + str(broadcasts.keys()) + " > ")
+    input_broadcast = input("Sendung? " + str(broadcasts.keys()) + " > ")
     try:
-        broadcast = str( broadcasts[input_broadcast])
+        broadcast = str(broadcasts[input_broadcast])
     except:
         pass
 
 ## Set episode number
 
-input_number = raw_input("Folgenzahl? (z.B. 197) > ")
+input_number = input("Folgenzahl? (z.B. 197) > ")
 
 newfilename = str(year) + str(month) + str(day) + '_' + broadcast + input_number
 if DEBUG == False:
     os.system("opusenc %s/%s.flac %s/%s.opus" % (audiofiles_folder, newfilename, audiofiles_folder, newfilename))
     #os.system("mv %s/%s %s/%s.flac" % (audiofiles_folder, flacfile, audiofiles_folder, newfilename))
-print("\033[1m# FLAC-Datei umgewandelt \n# von '%s'\n# zu  '%s.opus'\033[0m" % (flacfile, newfilename ))
-
+print("\033[1m# FLAC-Datei umgewandelt \n# von '%s'\n# zu \033[0m\n%s/%s.opus" % (flacfile, audiofiles_folder, newfilename ))
 
 ## Convert .cue-file to .psc-file
 
@@ -108,7 +109,7 @@ psc_content = []
 last_line = ""
 last_title = ""
 for line in pre_psc_content:
-    title = re.findall(r'(<=\d\d:\d\d\:\d\d\.000\ )*', line)
+    title = re.findall(r'(<=\d\d:\d\d\:\d\d\.\d\d\d\ )*', line)
     if last_line != line and title != last_title:
         psc_content.append(line)
     last_line = line
@@ -118,4 +119,20 @@ if DEBUG == False:
     with open(audiofiles_folder + "/" + newfilename + ".psc", "w") as psc_file:
         psc_file.write('\n'.join(psc_content))
 
-print("\033[1m# Playlist-Datei umgewandelt \n# von '%s'\n# zu  '%s.psc'\033[0m" % (listfile, newfilename))
+print("\033[1m# Playlist-Datei umgewandelt \n# von '%s'\n# zu\033[0m\n%s/%s.psc" % (listfile, audiofiles_folder, newfilename))
+
+
+## Get shownotes from etherpad
+
+pad_id = ''
+while pad_id == '':
+    pad_id = input("\033[1mEtherpad: Pad ID? (z.B. LL220) > \033[0m")
+
+pad_url = etherpad_url + "/" + pad_id + "/export/txt"
+with urllib.request.urlopen(pad_url) as ep_content:
+    ep_content = ep_content.read().decode('utf-8')
+    print('\n\033[1m––– Content of %s –––\033[0m' % (pad_url))
+    for line in ep_content.split("\n"):
+        if not re.match(r"\s+.*", line):
+            print(line)
+    print('\033[1m––– End –––\033[0m\n')
